84b49e972670b4b010b4b05731e0a06f464df9f7
Subhamdas20 <subham21.sd@gmail.com>
2021-10-18 21:41:49 +0530

    changing variable names

--- src/com/bz/wagecalculation/PartTimeWageCalculation.java
+++ src/com/bz/wagecalculation/PartTimeWageCalculation.java
@@ -2,10 +2,10 @@ package com.bz.wagecalculation;
 
 public class PartTimeWageCalculation {
     public static void main(String[] args) {
-        int wageperhour = 20;
-        int hoursperday = 8;
-        int hoursparttime = hoursperday/2;
-        System.out.println("The Daily Wage of Employee = " + wageperhour*hoursperday + " Rs");
-        System.out.println("The Daily Wage of Part Time Employee = " + wageperhour*hoursparttime + " Rs");
+        int wagePerHour = 20;
+        int hoursPerDay = 8;
+        int hoursPartTime = hoursPerDay/2;
+        System.out.println("The Daily Wage of Employee = " + wagePerHour*hoursPerDay + " Rs");
+        System.out.println("The Daily Wage of Part Time Employee = " + wagePerHour*hoursPartTime + " Rs");
     }
 }
