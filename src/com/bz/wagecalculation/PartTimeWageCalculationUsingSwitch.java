ae2407545aa587cd965ffaab9da93bb7b22f7f0e
Subhamdas20 <subham21.sd@gmail.com>
2021-10-18 21:43:49 +0530

    changing variable names

--- src/com/bz/wagecalculation/PartTimeWageCalculationUsingSwitch.java
+++ src/com/bz/wagecalculation/PartTimeWageCalculationUsingSwitch.java
@@ -5,14 +5,14 @@ public class PartTimeWageCalculationUsingSwitch {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 1 for Full Time Wage Calculation\nEnter 2 for Part Time Wage Calculation");
         int userInput = sc.nextInt();
-        int wageperhour = 20;
-        int hoursperday = 8;
-        int hoursparttime = hoursperday/2;
+        int wagePerHour = 20;
+        int hoursPerDay = 8;
+        int hoursPartTime = hoursPerDay/2;
         switch(userInput)
         {
-            case 1:System.out.println("The Daily Wage of Employee = " + wageperhour * hoursperday + " Rs");
+            case 1:System.out.println("The Daily Wage of Employee = " + wagePerHour * hoursPerDay + " Rs");
                 break;
-            case 2 :System.out.println("The Daily Wage of Part Time Employee = " + wageperhour * hoursparttime + " Rs");
+            case 2 :System.out.println("The Daily Wage of Part Time Employee = " + wagePerHour * hoursPartTime + " Rs");
                 break;
             default : System.out.println("Invalid Input From user");
         }
